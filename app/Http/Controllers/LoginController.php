5a84428a3144f3d3f40c10a90cb6169a3163954d
tasvishwas1 <tas.vishwas@gmail.com>
2023-02-06 19:48:19 +0530

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Product;
+use Validator;
+
+class LoginController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $request['password'] = bcrypt($request['password']);
+        
+        $user = User::create($request->all());
+
+        $token = $user->createToken('Token')->accessToken;
+
+        return response()->json(['token' => $token, 'userData' => $user], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if(auth()->attempt($data)) {
+            
+            $token = auth()->user()->createToken('Token')->accessToken;
+            return response()->json(['token' => $token], 200);
+
+        } else {
+
+            return response()->json(['error' => 'Unauthenticated'], 401);
+
+        }
+    }
+
+    public function products(Request $request)
+    {
+        $user = auth()->user();
+        $user_id = $user->id;
+        $products = Product::where('user_id', $user_id)->get();
+        return response()->json(['products' => $products], 200);
+    }
+
+    public function userInfo(Request $request)
+    {
+        $user = auth()->user();
+        return response()->json(['user' => $user], 200);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/// Passport Authentication Routes...
+Route::post('register', [LoginController::class, 'register']);
+Route::post('login', [LoginController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('get-user', [LoginController::class, 'userInfo']);
+    Route::get('products', [LoginController::class, 'products']);
+});
